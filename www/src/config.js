c14e2a08f1ac926d87619f52c6346c739d3ed11e
umair-f17 <umairf17iiui@gmail.com>
2022-03-28 15:59:15 +0500

    Update frontend config

--- www/src/config.js
+++ www/src/config.js
@@ -3,7 +3,7 @@
 
 const config = {
   "aws_user_pools_web_client_id": "2ffoaj6vpojskdf01af3njcp17",     // CognitoClientID
-  "api_base_url": "https://4axl6ten2i.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "api_base_url": "https://4axl6ten2i.execute-api.us-east-1.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
   "cognito_hosted_domain": "myappumair-sam-app.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
   "redirect_url": "https://master.d23khr7drzga2j.amplifyapp.com"                                      // AmplifyURL
 };
